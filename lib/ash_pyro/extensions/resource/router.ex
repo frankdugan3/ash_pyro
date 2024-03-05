ae374e2df7b94c60de871043325775761dd6a4d7
Frank Dugan III <frank3@duganator.com>
2024-03-05 11:05:39 -0600

    feat: add simple path sorter

--- lib/ash_pyro/extensions/resource/router.ex
+++ lib/ash_pyro/extensions/resource/router.ex
@@ -2,6 +2,7 @@ defmodule AshPyro.Extensions.Resource.Router do
   @moduledoc """
   Tooling to generate routes for AshPyro's LiveView page DSL.
   """
+  require Logger
 
   @doc """
   Generates live routes for a given LiveView, resource and page.
@@ -28,7 +29,7 @@ defmodule AshPyro.Extensions.Resource.Router do
     pyro_page = AshPyro.Extensions.Resource.Info.page_for(resource, page_name)
 
     for %{path: path, live_action: live_action} <-
-          pyro_page.live_actions do
+          Enum.sort(pyro_page.live_actions, &sort_routes/2) do
       path = Enum.join(["" | path], "/")
 
       quote do
@@ -39,6 +40,43 @@ defmodule AshPyro.Extensions.Resource.Router do
           as: unquote(page_name)
         )
       end
+    end
+  end
+
+  defp sort_routes(%{path: left}, %{path: right}) do
+    compare_paths(left, right)
+  end
+
+  defp compare_paths(left, right) do
+    if left == right do
+      Logger.warning("duplicate paths: #{inspect(left)}")
+    end
+
+    case {left, right} do
+      # dynamic segments can be compared
+      {[":" <> left_segment | left_rest], [":" <> right_segment | right_rest]} ->
+        compare_paths([left_segment | left_rest], [right_segment | right_rest])
+
+      # shorter paths should go last
+      {[], _} ->
+        false
+
+      {_, []} ->
+        true
+
+      # dynamic segments must come after statuc segments
+      {[":" <> _ | _], _} ->
+        false
+
+      {_, [":" <> _ | _]} ->
+        true
+
+      # identical segments must be compared on sub-path
+      {[left_segment | left], [right_segment | right]} when left_segment == right_segment ->
+        compare_paths(left, right)
+
+      {[left_segment | _], [right_segment | _]} ->
+        left_segment >= right_segment
     end
   end
 end
